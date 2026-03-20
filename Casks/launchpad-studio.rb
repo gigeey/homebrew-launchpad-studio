68042df1665dce4062da0a9d76a9e651b294c92f
eandrew45 <ebongandrew.ea@gmail.com>
2026-03-20 00:17:32 -0700

    Update: renamed app, dark mode icons, Gigeey branding

--- Casks/launchpad-studio.rb
+++ Casks/launchpad-studio.rb
@@ -1,19 +1,19 @@
 cask "launchpad-studio" do
   version "0.1.0"
-  sha256 "f9f8133817bddb128da9b82c942134000d7c923ac04cbe4dcd3406e44c1f6952"
+  sha256 "1594a99f04f21bee5823a727b87d95ecab1adf8c70ac8f6fd5568a345770d9af"
 
-  url "https://github.com/gigeey/launchpad-studio-releases/releases/download/v#{version}/launchpad_studio_#{version}_universal.dmg"
+  url "https://github.com/gigeey/launchpad-studio-releases/releases/download/v#{version}/Launchpad_Studio_#{version}_universal.dmg"
   name "Launchpad Studio"
   desc "Launchpad Studio desktop application"
   homepage "https://github.com/gigeey/launchpad-studio-releases"
 
   depends_on macos: ">= :catalina"
 
-  app "launchpad_studio.app"
+  app "Launchpad Studio.app"
 
   zap trash: [
-    "~/Library/Application Support/com.ebongandrew.launchpad-studio",
-    "~/Library/Caches/com.ebongandrew.launchpad-studio",
-    "~/Library/Preferences/com.ebongandrew.launchpad-studio.plist",
+    "~/Library/Application Support/com.gigeey.launchpad-studio",
+    "~/Library/Caches/com.gigeey.launchpad-studio",
+    "~/Library/Preferences/com.gigeey.launchpad-studio.plist",
   ]
 end
