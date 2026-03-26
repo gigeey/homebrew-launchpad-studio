550d860848281dd111f0e93244495494f311b6d8
eandrew45 <ebongandrew.ea@gmail.com>
2026-03-26 07:20:48 -0700

    Update launchpad-studio v0.3.0 SHA (crash fix rebuild)

--- Casks/launchpad-studio.rb
+++ Casks/launchpad-studio.rb
@@ -1,6 +1,6 @@
 cask "launchpad-studio" do
   version "0.3.0"
-  sha256 "29078c523716cd63f764dbd67d6312ea898e08c51ebd3717962a6ddefbdad9c5"
+  sha256 "281aaf505443a9fa9f7ec01e5be9f3b51a9a39abe011dad3490ca82bf654b639"
 
   url "https://github.com/gigeey/launchpad-studio-releases/releases/download/v#{version}/Launchpad_Studio_#{version}_universal.dmg"
   name "Launchpad Studio"
