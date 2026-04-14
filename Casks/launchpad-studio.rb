773c7df004fd63145f735cbb6af428055c5e7e18
eandrew45 <ebongandrew.ea@gmail.com>
2026-04-14 08:36:43 -0700

    Update launchpad-studio to v0.15.0

--- Casks/launchpad-studio.rb
+++ Casks/launchpad-studio.rb
@@ -1,6 +1,6 @@
 cask "launchpad-studio" do
-  version "0.14.0"
-  sha256 "e633b666020b67513df430b23b58fd143adb2126cb6bad1a9a9a43db8223ffcc"
+  version "0.15.0"
+  sha256 "d849d77daff874440582aa1c42b84b52f44762bd1c94fc6207042f4f44c462c0"
 
   url "https://github.com/gigeey/launchpad-studio-releases/releases/download/v#{version}/Launchpad_Studio_universal.dmg"
   name "Launchpad Studio"
